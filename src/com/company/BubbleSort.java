43e073f39f63abcae9c868dc1a6aa9a1bca10e7b
BT_1N3_29@bktech.local <BT_1N3_29@bktech.local>
2019-01-03 08:40:42 -0500

    Initial commit

---
+++ src/com/company/BubbleSort.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class BubbleSort {
+
+    public static int Swap() {
+        int constantPos = 0;
+        int changingPos = 1;
+        int temp;
+
+        for (temp, changingPos <= arr.length(), changingPos++) {
+            if (constantPos > changingPos) {
+                temp = changingPos;
+                changingPos = constantPos;
+                constantPos = temp;
+            }
+        }
+    }
+
+
+
+}
